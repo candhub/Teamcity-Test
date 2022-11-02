20b05109c8471d590c5b57254eddafb150b9010c
Ben <develop.cand@gmail.com>
2022-11-02 15:19:27 +0900

    Update code

---
+++ Teamcity Sample/Sample.cs
@@ -0,0 +1,5 @@
+﻿public class Sample
+{
+    public int Get_num()
+        => 5;
+}
--- Teamcity Test/UnitTest1.cs
+++ Teamcity Test/UnitTest1.cs
@@ -1,7 +1,13 @@
+using FluentAssertions;
+
 namespace Teamcity_Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1() { }
+    public void Test1()
+    {
+        var sut = new Sample();
+        sut.Get_num().Should().Be(5);
+    }
 }
